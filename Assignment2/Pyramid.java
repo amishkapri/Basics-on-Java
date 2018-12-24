cf8a597372da4ef3bae55e2cb8d14ff73bc10900
amishkapri <amishkapri24@gmail.com>
2018-12-24 22:12:45 +0100

    Second Commit

--- Assignment2/Pyramid.java
+++ Assignment2/Pyramid.java
@@ -12,7 +12,7 @@
 
 import acm.graphics.*;
 import acm.program.*;
-import java.awt.*;
+// import java.awt.*;
 
 public class Pyramid extends GraphicsProgram {
 
@@ -24,12 +24,25 @@ public class Pyramid extends GraphicsProgram {
 
 /** Number of bricks in the base of the pyramid */
 	private static final int BRICKS_IN_BASE = 14;
-	
-	public void run() {
-		getWidth();
-		getHeight();
-		GRect Stone = new GRect(100,450,BRICK_WIDTH,BRICK_HEIGHT);
-		add(Stone);
+/* 
+
+*/
+	public void run() {		
+		int h = BRICK_HEIGHT ;   // giving variable to the height of brick
+		double b = getWidth() / BRICK_WIDTH ;  // finding the total number of bricks that can be kept in the baseline
+		double k = (b-BRICKS_IN_BASE)/ 2 ;   // finding the point to put the first brick in the baseline
+	    double x = k * BRICK_WIDTH ;        // finding the x-co-ordinate
+	    double y = getHeight()- h ;        // finding the y-co-ordinate
+	    double m = (BRICK_WIDTH)/2 ;      // finding the midpoint of the brick
+	    for(int j=0; j < (BRICKS_IN_BASE); j++) {   // this for loop is to move across the vertical line
+            double n = BRICKS_IN_BASE - j ;
+            for(int i=0;i<n;i++) { // thid for loop is to move across the horizontal path
+            	double p = x + (m *j) + (BRICK_WIDTH * i);   	// finding a common technique to change the x co-ordinate 
+                double l = y - (h*j);                          // finding a common technique to change the y-co-ordinate
+            	GRect box = new GRect(p,l,BRICK_WIDTH,BRICK_HEIGHT);   
+            	add(box);
+            }
+	    }
 	}
 }
 
--- Assignment2/Target.java
+++ Assignment2/Target.java
@@ -12,6 +12,22 @@ import java.awt.*;
 
 public class Target extends GraphicsProgram {	
 	public void run() {
-		/* You fill this in. */
+		double x = getWidth()/2 ;
+		double y = getHeight() / 2;
+		GOval A = filledCircle(x, y, 72.0,Color.RED);
+		GOval B = filledCircle(x,y,46.8,Color.WHITE);
+		GOval C = filledCircle(x,y,21.6,Color.RED);
+		add(A);
+		add(B);
+		add(C);
+	}
+/* Defining a common formula to draw the circle because we are going to make three circles
+ * 	with same center but different radius. 
+ */
+	private GOval filledCircle(double p, double q, double r, Color color) { 
+		GOval Circle = new GOval(p-r,q-r, 2*r, 2*r) ;
+		Circle.setFilled(true);
+		Circle.setColor(color);
+		return(Circle);
 	}
 }
